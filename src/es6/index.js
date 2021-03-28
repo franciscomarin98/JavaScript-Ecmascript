9188050b39b1786bc4fdec093654be45d1a6f865
francisco-1998 <francmarinc@gmail.com>
2021-03-28 14:41:38 -0500

    Let, Const, Multiline, Spread Operator and Destructuring

--- src/es6/index.js
+++ src/es6/index.js
@@ -18,9 +18,40 @@ withES6();
 withES6('Pedro', 23, 'España');
 
 //2.- Template String
-let  hello = 'Hello';
+let hello = 'Hello';
 let world = 'World';
-let normalPhrase = hello+ ' ' + world;
-let epicPhrase = `${hello} ${world}`;
+let normalPhrase = hello + ' ' + world; //After
+let epicPhrase = `${hello} ${world}`;  //Before
 console.log(normalPhrase)
 console.log(epicPhrase)
+
+// 3.- Multiline
+let lorem = 'Lorem ipsum dolor sit amet, consectetur \n'
+    + 'otra frase épica';
+console.log(lorem)
+
+let lorem2 = `Lorem ipsum dolor sit amet, consectet
+y asi se hace con templates strings`;
+console.log(lorem2)
+
+//4.- Destructuring
+
+let person = {
+    'name': 'John',
+    'age': 34,
+    'country': 'ES'
+}
+
+console.log(person.name, person.age);
+let { name, age } = person;
+console.log(name, age);
+
+//5.- Spread Operator
+
+let team1 = ['John', 'Maria'];
+let team2 = ['Jose', 'Pedro'];
+let bigTeam = ['Francisco', ...team1, ...team2];
+console.log(bigTeam);
+
+//6.- Var, Let, Const
+//Esto de aqui son las formas que se usan para declarar variables,la palabra 'var' no se recomienda usarla actualmente en Javascript
